08d133d9102413bfa9a969b6e83085e2db9d5e45
Tadas Talaikis <xenu256@users.noreply.github.com>
2018-09-12 22:39:03 +0300

    upd

--- tests/index.js
+++ tests/index.js
@@ -1,16 +1,15 @@
-const fs = require('fs')
-const path = require('path')
-const chai = require('chai')
-const chaiAsPromised = require('chai-as-promised')
-chai.should()
-chai.expect()
-const assert = chai.assert
-chai.use(chaiAsPromised)
-const url = 'http://localhost:5000/'
-const request = require('supertest')(url)
-const faker = require('faker')
-const apiBenchmark = require('api-benchmark')
-const { read } = require('./fileFunctions')
+const fs = require('fs');
+const path = require('path');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const should = chai.should();
+const expect = chai.expect();
+chai.use(chaiAsPromised);
+const url = 'http://localhost:5000/';
+const request = require('supertest')(url);
+const faker = require('faker');
+const apiBenchmark = require('api-benchmark');
+const { read } = require('./fileFunctions');
 
 describe('user management', () => {
   it('returns all users', (done) => {
@@ -20,24 +19,24 @@ describe('user management', () => {
       .send({ query: '{ allUsers { nodes { email } } }'})
       .expect(200)
       .end((err, res) => {
-        if (err) return done(err)
-        res.should.have.property('body')
-        res.body.data.allUsers.should.have.property('nodes')
-        done()
-      })
-  })
+        if (err) return done(err);
+        res.should.have.property('body');
+        res.body.data.allUsers.should.have.property('nodes');
+        done();
+      });
+  });
 
   it('signup works for anyone', (done) => {
     request.post('graphql')
       .send({ query: `mutation { signup(input: { email: "${faker.internet.email()}", password: "${faker.internet.password()}" }) { clientMutationId } }` })
       .expect(200)
       .end((err, res) => {
-        if (err) return done(err)
-        res.should.have.property('body')
-        res.body.data.should.have.property('signup')
-        done()
-      })
-  })
+        if (err) return done(err);
+        res.should.have.property('body');
+        res.body.data.should.have.property('signup');
+        done();
+      });
+  });
 
   it('multi user signup is okay', (done) => {
     for (let i = 0; i < 100; i++) {
@@ -45,116 +44,107 @@ describe('user management', () => {
         .send({ query: `mutation { signup(input: { email: "${faker.internet.email()}", password: "${faker.internet.password()}" }) { clientMutationId } }` })
         .expect(200)
         .end((err, res) => {
-          if (err) return done(err)
-          res.should.have.property('body')
-          res.body.data.should.have.property('signup')
-        })
+          if (err) return done(err);
+          res.should.have.property('body');
+          res.body.data.should.have.property('signup');
+        });
     }
-    done()
-  })
+    done();
+  });
 
   it('doesn\'t allow to register for existing user', (done) => {
-    const email = faker.internet.email()
-    const password = faker.internet.password()
+    const email = 'foo@example.com';
+    const password = 'test';
     request.post('graphql')
       .send({ query: `mutation { signup(input: { email: "${email}", password: "${password}" }) { clientMutationId } }`})
       .expect(200)
       .send({ query: `mutation { signup(input: { email: "${email}", password: "${password}" }) { clientMutationId } }` })
       .expect(200)
       .end((err, res) => {
-        if (err) return done(err)
-        res.body.errors[0].should.have.property('message')
-        done()
-      })
-  })
+        if (err) return done(err);
+        res.body.errors[0].should.have.property('message');
+        done();
+      });
+  });
 
   it('login ok for users', (done) => {
-    const email = faker.internet.email()
-    const password = faker.internet.password()
+    const email = 'foo2@example.co.uk';
+    const password = 'test';
+
     request.post('graphql')
       .send({ query: `mutation { signup(input: { email: "${email}", password: "${password}" }) { clientMutationId } }` })
       .expect(200)
       .end((err, res) => {
-        if (err) return done(err)
-        res.body.data.should.have.property('signup')
-      })
-    const token = fs.readFileSync(path.resolve(__dirname, './last_validation_token'))
+        if (err) return done(err);
+        res.body.data.should.have.property('signup');
+      });
+    const token = fs.readFileSync(path.resolve(__dirname, './last_validation_token'));
     request.post('graphql')
       .send({ query: `mutation {validate(input: { tok: "${token.toString()}" }) {clientMutationId}}` })
       .expect(200)
       .end((err, res) => {
-        if (err) return done(err)
-        res.body.data.should.have.property('validate')
-      })
+        if (err) return done(err);
+        res.body.data.should.have.property('validate');
+      });
     request.post('graphql')
       .send({ query: `mutation {login(input: {email: "${email}", password: "${password}"}) {json}}` })
       .expect(200)
       .end((err, res) => {
-        if (err) return done(err)
-        res.login.json.should.have.property('token')
-      })
-      done()
-  })
+        if (err) return done(err);
+        res.login.json.should.have.property('token');
+        done();
+      });
+  });
 
   it('login not ok for non-users', (done) => {
     request.post('graphql')
       .send({ query: `mutation {login(input: {email: "${faker.internet.email()}", password: "${faker.internet.password()}"}) {json}}` })
       .expect(200)
       .end((err, res) => {
-        if (err) return done(err)
-        console.log(res.body)
-        res.body.errors[0].should.have.property('message')
-        done()
-      })
-  })
+        if (err) return done(err);
+        res.body.errors[0].message.should.be.equal('Invalid role.');
+        done();
+      });
+  });
 
   it('wrong validation uuids not accepted', (done) => {
     request.post('graphql')
       .send({ query: 'mutation {validate(input: { tok: "a31938c6-b29d-4413-a4a4-c0c844356527" }) {clientMutationId}}' })
       .expect(200)
       .end((err, res) => {
-        if (err) return done(err)
-        res.body.errors[0].should.have.property('message')
-        done()
-      })
-  })
+        if (err) return done(err);
+        res.body.errors[0].message.should.be.equal('No such validation token.');
+        done();
+      });
+  });
 
   it('user update works', (done) => {
-    const email = faker.internet.email()
-    const password = faker.internet.password()
-
+    const email = 'foo2@example.co.uk';
+    const password = 'test';
     request.post('graphql')
-      .send({ query: `mutation { signup(input: { email: "${email}", password: "${password}" }) { clientMutationId } }` })
-      .expect(200)
       .send({ query: `mutation {updateUserInfo(input: {mail: "${email}", password: "${password}", firstname: "First", lastname: "Last", _about: "About me"}) {clientMutationId}}` })
       .expect(200)
       .end((err, res) => {
-        if (err) return done(err)
-        console.log(res.body)
-        done()
-      })
-  })
+        if (err) return done(err);
+        res.body.data.should.have.property('updateUserInfo');
+        done();
+      });
+  });
 
   it('user delete works', (done) => {
-    const email = faker.internet.email()
-    const password = faker.internet.password()
-
+    const email = 'foo2@example.co.uk';
+    const password = 'test';
     request.post('graphql')
-      .send({ query: `mutation { signup(input: { email: "${email}", password: "${password}" }) { clientMutationId } }` })
-      .expect(200)
       .send({ query: `mutation {deleteUserAccount(input: {mail: "${email}", password: "${password}"}) {clientMutationId}}` })
       .expect(200)
       .end((err, res) => {
-        if (err) return done(err)
-        console.log(res.body)
-        done()
-      })
-  })
+        if (err) return done(err);
+        res.login.json.should.have.property('token');
+        done();
+      });
+  });
 
   /*
-  it('verification should work', (done) => {
-  })
-
   it('password reset works for user', (done) => {
     request.post('graphql')
       .send({ query: 'mutation { signup(input: { email: "foo60@example.com", password: "123456" }) { clientMutationId } }' })
@@ -186,7 +176,7 @@ describe('user management', () => {
     it('change role not available for ordinary users', (done) => {
     })
   })*/
-})
+});
 
 /*describe('cms', () => {
 })
@@ -196,13 +186,13 @@ describe('blockchain', () => {
 */
 
 const service = {
-  server: `${url}`
-}
+  server: `${url}`,
+};
 
 const routes = {
   route: { name: 'Registration', route: 'graphql', method: 'post', data: { query: `mutation { signup(input: { email: "${faker.internet.email()}", password: "${faker.internet.password()}" }) { clientMutationId } }` } },
-}
+};
 
 apiBenchmark.measure(service, routes, (err, res) => {
-  console.log(`Mean for ${res.server.route.name}: ${res.server.route.stats.mean}`)
-})
+  console.log(`Mean for ${res.server.route.name}: ${res.server.route.stats.mean}`);
+});
